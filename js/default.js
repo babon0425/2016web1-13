2cbd2534c2e25cc888be4d74a04d489b5d9273f0
g1527061 <babon0425@gmail.com>
2016-07-04 17:47:37 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -10,9 +10,9 @@ var now = new Date();
 var target = new Date(2020,7,24,0,0,0,0);
 var diff = target.getTime() - now.getTime();
 var counter = saparate_time(diff);
-document.getElementById("CountDown").TextContent = 
-'東京オリンピックまであと' + 
+document.getElementById("CountDown").textContent = 
+"'東京オリンピックまであと' + 
  counter[3] + '日' +
  counter[2] + '時間' +
  counter[1] + '分' +
- counter[0] + '秒';
+ counter[0] + '秒'";
