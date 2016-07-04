87be1af93c7d490fbfc55233a0651c6cacdf8ed3
g1527061 <babon0425@gmail.com>
2016-07-04 17:17:13 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,8 +1,8 @@
 var separate_time = function(time){
-  var sec = time.getSecond();
+  var sec = time.getSeconds();
   var min = time.getMinutes();
   var hours = time.getHours();
-  var days = time.getDays();
+  var days = time.getDate();
   var month = time.getMonth();
   var year = time.getFullYear();
   return [sec,min,hours,days,month,year];
